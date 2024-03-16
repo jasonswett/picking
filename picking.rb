31b07347782dede0768fc67c4823da97eef4a509
Jason Swett <jason@benfranklinlabs.com>
2024-03-16 14:15:34 -0400

    Refactoring.

--- picking.rb
+++ picking.rb
@@ -2,28 +2,31 @@ require_relative "order_collection"
 require_relative "picker"
 require_relative "fitness_score"
 
+def assign_orders(pickers, orders)
+  while orders.any?
+    pickers.map do |picker|
+      order = orders.pop
+      picker.orders << order if order
+    end
+  end
+
+  puts "-" * 80
+
+  pickers.each do |picker|
+    puts "Picker ##{picker.number}"
+    puts picker.orders_by_channel
+    puts
+  end
+
+  puts "Fitness score: #{FitnessScore.new(pickers).value}"
+end
+
+pickers = Picker.generate(3)
+
 order_collection = OrderCollection.new(
   amazon: 2,
   ebay: 2,
   back_market: 2
 )
 
-pickers = Picker.generate(3)
-flattened_orders = order_collection.flatten.shuffle
-
-while flattened_orders.any?
-  pickers.map do |picker|
-    order = flattened_orders.pop
-    picker.orders << order if order
-  end
-end
-
-puts "-" * 80
-
-pickers.each do |picker|
-  puts "Picker ##{picker.number}"
-  puts picker.orders_by_channel
-  puts
-end
-
-puts "Fitness score: #{FitnessScore.new(pickers).value}"
+assign_orders(pickers, order_collection.flatten)
